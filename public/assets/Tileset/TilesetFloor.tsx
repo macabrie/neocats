675a7fb31bbd574f6cb0fb812ece6c2256ed99aa
macabrie <bliang95@gmail.com>
2022-09-30 23:18:43 -0400

    town tileset

---
+++ public/assets/Tileset/TilesetFloor.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="TilesetFloor" tilewidth="16" tileheight="16" tilecount="572" columns="22">
+ <image source="TilesetFloor.png" width="352" height="417"/>
+ <wangsets>
+  <wangset name="Dark Green Road" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="165" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="166" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="167" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="187" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="189" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="209" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="210" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="211" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+  <wangset name="Light Green Road" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="154" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="155" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="156" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="176" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="177" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="178" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="198" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="199" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="200" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
---
+++ public/assets/Tileset/TilesetNature.tsx
@@ -0,0 +1,164 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="TilesetNature" tilewidth="16" tileheight="16" tilecount="336" columns="16">
+ <image source="TilesetNature.png" width="256" height="336"/>
+ <tile id="139">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="155">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="160">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="161">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="162">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="163">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="164">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="165">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="166">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="167">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="168">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="169">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="170">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="171">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="288">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="289">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="290">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="297">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="298">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="299">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="304">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="305">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="306">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="313">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="314">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="315">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="320">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="321">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="322">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="329">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="330">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="331">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
